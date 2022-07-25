5adc08fa9c1d4a479539d6e6352d22b10597d77e
Andreii-Denysenko <a.denisenko2111@gmail.com>
2022-07-25 14:44:42 +0300

    solution tasck 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const text = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+btnChangeColor.addEventListener('click', changeColor );
+
+function changeColor (){
+document.body.style.backgroundColor = getRandomHexColor();
+text.textContent = getRandomHexColor();
+}
